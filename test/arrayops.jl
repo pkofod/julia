c52dba706a9ed41ab576464e91d4123e1f54470b
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-04-29 09:03:01 +0200

    Revert "Send dummy error message when setindex!(::ReshapedRange) test fails-to-fail"
    This reverts commit f5810f26adab8c241909c4a01fbcea29c2d3eaf3.

--- test/arrayops.jl
+++ test/arrayops.jl
@@ -131,7 +131,6 @@ a = reshape(1:20, 5, 4)
 for idx in ((3,), (2,2), (Base.ReshapedIndex(1),))
     try
         a[idx...] = 7
-        error("wrong error")
     catch err
         @test err.msg == "indexed assignment fails for a reshaped range; consider calling collect"
     end
